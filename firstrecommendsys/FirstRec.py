b52dfb51e653bfd946caf16b0e03b3ad3be62e9f
zhouyu1 <zhouyu1@unicdata.com>
2023-02-20 20:04:50 +0800

    init

---
+++ firstrecommendsys/FirstRec.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import os
+import json
+import random
+import math
+
+class FirstRec:
+    """
+    初始化函数：
+        filePath: 原始文件路径
+        seed: 产生随机数的种子
+        k: 选取的近邻用户个数
+        nitems: 为每个用户推荐的电影数
+    """
+    def __init__(self,file_path,seed,k,n_items):
+        pass
